9b97e217a7a786225d06eb35fe8bfa2fc13b24f8
Jakub Zaorálek <jakub.zaoralek@ness.com>
2016-06-01 10:03:12 +0200

    pom.xml and first controller

---
+++ scb-data-service/src/main/java/com/jzaoralek/scb/dataservice/controller/CourseApplicationController.java
@@ -0,0 +1,34 @@
+package com.jzaoralek.scb.dataservice.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Rest Controller for applications to swimming courses.
+ */
+
+@RestController
+@RequestMapping("/course-applications")
+public class CourseApplicationController {
+	private static final Logger LOG = LoggerFactory.getLogger(CourseApplicationController.class);
+
+	/**
+	 * Returns cource application for uuid.
+	 * @param uuid
+	 * @return
+	 */
+    @RequestMapping(value = "/{uuid}", method = RequestMethod.GET)
+    public String read(@PathVariable("uuid") String uuid) {
+
+    	if (LOG.isDebugEnabled()) {
+    		LOG.debug("read claim: {}", uuid);
+        }
+
+    	// just for test purposes
+        return "uuid" + uuid;
+    }
+}
