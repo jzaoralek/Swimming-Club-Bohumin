84d7fefbf409bb4e07946d25c8016fde221e9851
Jakub Zaoralek <jakub.zaoralek@gmail.com>
2018-02-05 21:30:05 +0100

    email detail, list of recipients to grid with filter

--- scb-ui-zk/src/main/java/com/jzaoralek/scb/ui/pages/email/EmailDetailWinVM.java
+++ scb-ui-zk/src/main/java/com/jzaoralek/scb/ui/pages/email/EmailDetailWinVM.java
@@ -1,11 +1,15 @@
 package com.jzaoralek.scb.ui.pages.email;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.util.resource.Labels;
 import org.zkoss.zul.Window;
 
@@ -21,14 +25,17 @@ import com.jzaoralek.scb.ui.common.vm.BaseVM;
 public class EmailDetailWinVM extends BaseVM {
 
 	private Set<String> emailAddressSet;
+	private Set<String> emailAddressSetBase;
 	private String messageSubject;
 	private String messageText;
+	private final EmailAddressFilter filter = new EmailAddressFilter();
 
 	@SuppressWarnings("unchecked")
 	@Override
 	@Init
 	public void init() {
 		this.emailAddressSet = (Set<String>) WebUtils.getArg(WebConstants.COURSE_PARTIC_CONTACT_LIST_PARAM);
+		this.emailAddressSetBase = this.emailAddressSet;
 		this.pageHeadline = Labels.getLabel("txt.ui.common.NovaZprava");
 	}
 	
@@ -40,6 +47,12 @@ public class EmailDetailWinVM extends BaseVM {
 		
 		WebUtils.showNotificationInfo(Labels.getLabel("msg.ui.info.messageSent"));
 		window.detach();
+	}
+	
+	@Command
+	@NotifyChange("emailAddressSet")
+	public void filterDomCmd() {
+		this.emailAddressSet = filter.getEmailListFiltered(this.emailAddressSetBase);
 	}
 	
 	public int getEmailAddressCount() {
@@ -68,5 +81,55 @@ public class EmailDetailWinVM extends BaseVM {
 
 	public Set<String> getEmailAddressSet() {
 		return emailAddressSet;
+	}
+	
+	public void setEmailAddressSet(Set<String> emailAddressSet) {
+		this.emailAddressSet = emailAddressSet;
+	}
+	
+	public EmailAddressFilter getFilter() {
+		return filter;
+	}
+	
+	public static class EmailAddressFilter {
+
+		private String address;
+		private String addressLc;
+
+		public boolean matches(String addressIn, boolean emptyMatch) {
+			if (address == null) {
+				return emptyMatch;
+			}
+			if (address != null && !addressIn.toLowerCase().contains(addressLc)) {
+				return false;
+			}
+			return true;
+		}
+
+		public Set<String> getEmailListFiltered(Set<String> courseList) {
+			if (courseList == null || courseList.isEmpty()) {
+				return Collections.<String>emptySet();
+			}
+			Set<String> ret = new HashSet<>();
+			for (String item : courseList) {
+				if (matches(item, true)) {
+					ret.add(item);
+				}
+			}
+			return ret;
+		}
+
+		public String getAddress() {
+			return address == null ? "" : address;
+		}
+		public void setAddress(String name) {
+			this.address = StringUtils.hasText(name) ? name.trim() : null;
+			this.addressLc = this.address == null ? null : this.address.toLowerCase();
+		}
+
+		public void setEmptyValues() {
+			address = null;
+			addressLc = null;
+		}
 	}
 }
