1e09847a795876ede8c732e4b7c5d1969f7f287b
Jakub Zaoralek <jakub.zaoralek@gmail.com>
2018-08-23 05:56:14 +0200

    check unique email in parent zone

--- scb-ui-zk/src/main/java/com/jzaoralek/scb/ui/pages/user/vm/UserRepresentativeVM.java
+++ scb-ui-zk/src/main/java/com/jzaoralek/scb/ui/pages/user/vm/UserRepresentativeVM.java
@@ -2,11 +2,15 @@ package com.jzaoralek.scb.ui.pages.user.vm;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.zkoss.bind.BindUtils;
+import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.util.resource.Labels;
+import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zul.Messagebox;
 
 import com.jzaoralek.scb.dataservice.domain.ScbUser;
 import com.jzaoralek.scb.dataservice.exception.ScbValidationException;
@@ -43,6 +47,29 @@ public class UserRepresentativeVM extends BaseVM {
 		} catch (ScbValidationException e) {
 			LOG.warn("ScbValidationException caught during storing user: " + this.user, e);
 			WebUtils.showNotificationError(e.getMessage());
+		}
+	}
+	
+	/**
+	 * Kontroluje pouziti emailu jako defaultniho prihlasovaciho jmena, pokud je jiz evidovano, nabidne predvyplneni hodnot zakonneho zastupce.
+	 * @param email
+	 * @param fx
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@NotifyChange("*")
+	@Command
+	public void validateUniqueUsernameCmd(@BindingParam("email") String email, @BindingParam("fx") final UserRepresentativeVM fx) {
+		// pokud existuje uzivatel se stejnym username jako je zadany email, zobrazit upozorneni a nepovolit vyplnit
+		final ScbUser scbUser = scbUserService.getByUsername(email);
+		if (scbUser != null && !this.user.getUsername().equals(email)) {
+			String question = Labels.getLabel("msg.ui.quest.participantRepresentativeExists",new Object[] {email, scbUser.getContact().getCompleteName()});			
+			Messagebox.show(question, Labels.getLabel("txt.ui.common.warning"), Messagebox.OK, Messagebox.EXCLAMATION, new org.zkoss.zk.ui.event.EventListener() {
+			    public void onEvent(Event evt) throws InterruptedException {
+			        // vymazat email
+			        fx.getUser().getContact().setEmail1("");
+			        BindUtils.postNotifyChange(null, null, fx, "*");
+			    }
+			});
 		}
 	}
 	
