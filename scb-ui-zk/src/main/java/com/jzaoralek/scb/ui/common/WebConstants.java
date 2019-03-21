f3138bcfecc25d20dedd78e6072673931ba568e5
Jakub Zaoralek <jakub.zaoralek@gmail.com>
2019-03-21 06:14:33 +0100

    open course participant in new tab

--- scb-ui-zk/src/main/java/com/jzaoralek/scb/ui/common/WebConstants.java
+++ scb-ui-zk/src/main/java/com/jzaoralek/scb/ui/common/WebConstants.java
@@ -23,6 +23,7 @@ public final class WebConstants {
 	public static final String WEB_TIME_SECONDS_PATTERN = "mm:ss.ms";
 
 	public static final String UUID_PARAM = "uuid";
+	public static final String NEW_TAB_PARAM = "newTab";
 	public static final String COURSE_COURSE_PARTIC_UUID_PARAM = "courseCourseParticUuid";
 	public static final String MODE_PARAM = "mode";
 	public static final String FROM_PAGE_PARAM = "fromPage";
