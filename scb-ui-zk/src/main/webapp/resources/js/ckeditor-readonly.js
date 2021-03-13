0a9c9c072dfad8a72c4afa7104ebea12efcae255
Jakub Zaoralek <jakub.zaoralek@gmail.com>
2021-03-13 21:33:02 +0100

    mail-sent-items, mail send list and detail UI

--- scb-ui-zk/src/main/webapp/resources/js/ckeditor-readonly.js
+++ scb-ui-zk/src/main/webapp/resources/js/ckeditor-readonly.js
@@ -4,9 +4,13 @@
 CKEDITOR.editorConfig = function(config) {
 	config.resize_enabled = false;
 	config.readOnly = true;
-	config.removePlugins = 'elementspath';
-	config.removePlugins = 'toolbar';
 	config.toolbar = 'Complex';
-	config.toolbar_Simple = [ ];
-	config.toolbar_Complex = [ ];
+	config.toolbar_Complex = [
+			[ 'Undo', 'Redo' ],
+			[ 'Bold', 'Italic', 'Underline', 'Subscript',
+					'Superscript', 'TextColor', 'BGColor', '-', 'Cut', 'Copy',
+					'Paste', '-', 'NumberedList',
+					'BulletedList'],
+			[ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ],
+			[ 'Format', 'Font', 'FontSize', 'Maximize' ] ];
 };
