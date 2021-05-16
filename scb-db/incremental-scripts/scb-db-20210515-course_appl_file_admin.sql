1f910a53273487cc9b3be24643a08fcaf5594af5
Jakub Zaoralek <jakub.zaoralek@gmail.com>
2021-05-16 18:14:37 +0200

    course application file admin incremental SQL script

--- scb-db/incremental-scripts/scb-db-20210515-course_appl_file_admin.sql
+++ scb-db/incremental-scripts/scb-db-20210515-course_appl_file_admin.sql
@@ -1,15 +1,1 @@
-CREATE TABLE mail_message_send (
-	uuid varchar(36),
-	mail_to VARCHAR(1000) NOT NULL,
-    mail_to_complete_name VARCHAR(1000) NULL,
-	mail_cc VARCHAR(1000) NULL,
-	mail_subject VARCHAR(1000) CHARACTER SET utf8,
-	mail_text LONGTEXT CHARACTER SET utf8,
-	success ENUM('0','1') NOT NULL DEFAULT '1',
-    description TEXT NULL,
-	attachments ENUM('0','1') NOT NULL DEFAULT '0',
-	html ENUM('0','1') NOT NULL DEFAULT '0',
-	modif_at TIMESTAMP NOT NULL,
-	modif_by varchar(36) NOT NULL,
-	PRIMARY KEY (uuid)
-);
+ALTER TABLE course_application_file_config MODIFY type ENUM('GDPR','HEALTH_INFO','HEALTH_EXAM', 'CLUB_RULES', 'OTHER') NOT NULL;
